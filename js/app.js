46c319b06fd85e399819dd7587fe6cd712f9430e
Lucas Augusto Vaz Nogueira <lucas.vaz.nogueira@avanade.com>
2023-07-11 01:24:29 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,13 @@
+console.log('Olá, Javascript!')
+
+var userName ='Lucas Nogueira'
+
+document.getElementById('user-name').innerHTML= userName
+
+//var nome = 'Mestre Yoda'
+//var idade = 100
+//var jedi = true
+
+//console.log(typeof nome)
+//console.log(typeof idade)
+//console.log(typeof jedi)
