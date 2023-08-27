49afdd5d46e132f1ceccd968e966db9a9ea02a67
Lucas Augusto Vaz Nogueira <lucas.vaz.nogueira@avanade.com>
2023-08-27 18:22:33 -0300

    commit de 2708

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 console.log('Olá, Javascript!')
 
-var userName ='Lucas Nogueira'
+var userName = 'Lucas Nogueira'
 
-document.getElementById('user-name').innerHTML= userName
+document.getElementById('user-name').innerHTML = userName
+
+// Variráveis//
 
 //var nome = 'Mestre Yoda'
 //var idade = 100
@@ -11,3 +13,49 @@ document.getElementById('user-name').innerHTML= userName
 //console.log(typeof nome)
 //console.log(typeof idade)
 //console.log(typeof jedi)
+
+// Operadores matemáticos //
+
+//var n1 = 7
+//var n2 = 2.5
+
+//console.log(typeof n1)
+//console.log(typeof n2)
+
+// + para somar
+// - para subtrair
+// * para multiplicar
+// / para dividir
+
+//var total = n1 / n2
+//console.log(total)
+
+// Operadores de comparação //
+
+//var v1 = 5
+//var v2 = '5'
+
+//var resultado = v1 !== v2
+//console.log(resultado)
+
+// Funções //
+
+//function soma(n1, n2) {
+//   console.log(n1 + n2)
+
+// }
+//soma(5, 100)
+
+//function boasVindas(nome) {
+//    alert(nome + ',Seja bem vindo(a)')
+// }
+
+
+ // boasVindas('Lucas')
+
+ function soma (n1 , n2) {
+    return n1 + n2
+ }
+
+  var resultado = soma(5,5)
+  console.log(resultado)
